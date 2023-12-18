84bf071a4e36993de32921b08d741abd6b38849e
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2023-12-18 11:44:14 +0700

    form

--- src/Lab07.java
+++ src/Lab07.java
@@ -1,3 +1,50 @@
-public class Lab07 {
+import java.util.Collection;
 
+public class Lab07 {
+    public  static void main(String[] args) {
+        set<Integer> mhai =new set<Integer>();
+        mhai.add(12);
+        mhai.add(12);
+        mhai.add(89);
+        System.out.println(mhai.size());
+        System.out.println(mhai.contains(12));
+        System.out.println(mhai.contains(120));
+        System.out.println();
+
+        set<Integer> c = new set<>();
+        c.add(12);
+        c.add(120);
+        set<Integer> mhoo = new set<Integer>();
+        mhoo.add(12);
+        mhoo.add(120);
+        mhoo.add(134);
+        System.out.println(mhoo.size());
+        System.out.println(mhoo.containsAll(c));
+        System.out.println(mhai.containsAll(mhoo));
+        System.out.println();
+
+        mhai.addAll(mhoo);
+        System.out.println(mhai.size());
+
+        System.out.println();
+        mhai.remove(89);
+        mhai.remove(28);
+        System.out.println(mhai.size());
+        System.out.println();
+
+        mhai.removeAll(mhoo);
+        System.out.println(mhai.size());
+
+        set<Integer> mhao = new set<Integer>();
+        mhao.add(134);
+
+        mhoo.retainAll(mhao);
+        System.out.println(mhoo.size());
+
+        System.out.println();
+        System.out.println(mhoo.size());
+        mhoo.clear();
+        System.out.println(mhoo.size());
+
+    }
 }
--- src/set.java
+++ src/set.java
@@ -2,75 +2,112 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.HashMap;
-public class set implements Set {
-
+public class set<E> implements Set<E> {
+    private static final Object DUMMY = new Object();
+    private HashMap<E, Object> map ;
 
     public set(){
-
+        map = new HashMap<>();
     }
 
     @Override
     public int size() {
-        return 0;
+        return map.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return map.isEmpty();
     }
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        return map.containsKey(o);
     }
 
     @Override
-    public Iterator iterator() {
-        return null;
-    }//unmentioned
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
+    }
 
     @Override
     public Object[] toArray() {
         return new Object[0];
-    }//unmentioned
+    }
 
     @Override
-    public boolean add(Object o) {
-        return false;
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(E e) {
+        if(!map.containsKey(e)) {
+            map.put(e, DUMMY);
+            return true;
+        }else return false;
     }
 
     @Override
     public boolean remove(Object o) {
+        if (contains(o)) {
+            map.remove(o);
+            return true;
+        }
         return false;
     }
 
     @Override
-    public boolean addAll(Collection c) {
-        return false;
+    public boolean containsAll(Collection<?> c) {
+        for (Object element : c) {
+            if (!contains(element)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        boolean modified = false;
+        Iterator<? extends E> iterator = (Iterator<? extends E>) c.iterator();
+        while (iterator.hasNext()) {
+            E element = iterator.next();
+            /* If add(element) returns true, แปลว่า the set was modified,
+            then modified will be set to true. */
+            modified |= this.add(element);
+        }
+
+        return modified;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean modified = false;
+        Iterator<HashMap.Entry<E, Object>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            HashMap.Entry<E, Object> entry = iterator.next();
+            if (!c.contains(entry.getKey())) {
+                iterator.remove();
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean modified = false;
+        for (Object element : c) {
+            modified |= remove(element);
+        }
+        return modified;
     }
 
     @Override
     public void clear() {
-
+        map.clear();
     }
 
-    @Override
-    public boolean removeAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public boolean containsAll(Collection c) {
-        return false;
-    }
-
-    @Override
-    public Object[] toArray(Object[] a) {
-        return new Object[0];
-    }//unmentioned
 }
